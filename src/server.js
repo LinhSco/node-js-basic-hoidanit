1f7446726fed8e3a23ad26fc7b196c5bfa53fd3f
Linh Sco <mydreamlinhsco@gmail.com>
2023-03-10 22:09:06 +0700

    video 7

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 // const express = require('express');
 import express from 'express';
 import configViewEngine from './configs/viewEngine.js';
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 configViewEngine(app);
 app.get('/', (req, res) => {
